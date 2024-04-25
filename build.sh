fd5a27ba7ef6815bb68ef16bcc1b840b24359a4d
SirDank <52797753+SirDank@users.noreply.github.com>
2024-04-25 20:16:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ echo "  > Enter project ( without .py ): "
 read project
 echo ""
 
-python3 -m nuitka --standalone --onefile --clang --warn-unusual-code --assume-yes-for-downloads --show-anti-bloat-changes --plugin-enable=pylint-warnings --noinclude-default-mode=nofollow --product-name="$project" --file-description="$project" --file-version=69.69.69.69 --product-version=69.69.69.69 $project.py
+python3 -m nuitka --standalone --onefile --onefile-tempdir-spec={TEMP}/dankware_{PID} --clang --warn-unusual-code --assume-yes-for-downloads --show-anti-bloat-changes --plugin-enable=pylint-warnings --noinclude-default-mode=nofollow --product-name="$project" --file-description="$project" --file-version=69.69.69.69 --product-version=69.69.69.69 $project.py
