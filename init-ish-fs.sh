546de783052cfcb04f7ba5ca36942ee3bd1ef3b1
SirDank <52797753+SirDank@users.noreply.github.com>
2024-04-26 17:48:48 +0530

    .

--- init-ish-fs.sh
+++ init-ish-fs.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 mount -o bind /dev dev/
-mount -o bind /lib lib/
-mount -o bind /proc proc/
-mount -o bind /run run/
-mount -o bind /sys sys/
-mount -o bind /usr usr/
-mount -o bind /var var/
+mount -o bind /proc/filesystems proc/filesystems
+mount -o bind /lib/modules lib/modules/
